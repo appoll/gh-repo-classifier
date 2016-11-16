e7c70f184aa2e667c9411a6e7199e2b56eff2c43
FlatErik90 <efl@informatik.uni-kiel.de>
2016-11-16 16:04:09 +0100

    updated for storing repo data into json files

--- data exploration/collect_data.py
+++ data exploration/collect_data.py
@@ -1,6 +1,6 @@
 import json
 import requests
-
+from requests.auth import HTTPBasicAuth
 
 class ExampleData:
 
@@ -8,14 +8,20 @@ class ExampleData:
 
         self.repo_data = []
 
-        with open("dev_repos.txt", 'r') as file:
+        with open("../data collection/dev/dev_repos_names.txt", 'r') as file:
             repos = file.readlines()
 
-        for repo in repos:
-            r = requests.get("https://api.github.com/repos/"+repo)
-            if r.status_code == 200:
-                self.objects.append((r.json(), 'DEV'))
+        header = {'X-Github-Username': 'FlatErik90', 'X-Github-API-Token': "36dc9f6e45d8019dafe001adf05c433f22e6782e"}
 
+        for repo in repos:
+            r = requests.get("https://api.github.com/repos/"+repo[:-1],auth=HTTPBasicAuth('FlatErik90','bvb90EF'))
+            print "https://api.github.com/repos/"+repo, r.status_code, r.json()
+            if r.status_code == 200:
+                self.repo_data.append((r.json(), 'DEV'))
+                with open("../data collection/dev/repos/"+repo[:-1].split('/')[1]+".json",'w') as file:
+                    jsonContent = str(r.json())
+                    file.write(jsonContent)
+                    file.close()
 
 
         print self.repo_data
