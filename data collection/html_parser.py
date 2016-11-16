3c7d0d9738e79670d5bd8084271537df7b45e6a2
appoll <paul.antton@gmail.com>
2016-11-16 14:05:01 +0100

    Make the html parser smarter and add hw example repos

--- data collection/html_parser.py
+++ data collection/html_parser.py
@@ -1,6 +1,35 @@
 import re
+from labels import Labels
 
 SIMILAR_RESULTS_COUNT = 10
+
+
+def read_and_store_repo_names(files, label):
+    repos_urls = label + "/" + label + "_repos_urls.txt"
+    repos_names = label + "/" + label + "_repos_names.txt"
+
+    file_repos_urls = open(repos_urls, 'w')
+    file_repos_names = open(repos_names, 'w')
+
+    for file in files:
+        lines = file.readlines()
+        file.close()
+        count = 0
+        for line in lines:
+            url = matching_urls.search(line)
+            similarity = matching_similarity.search(line)
+            if url != None:
+                url = url.group(1)
+                file_repos_urls.write(url)
+                file_repos_urls.write('\n')
+                if count < SIMILAR_RESULTS_COUNT:
+                    split = url.split("/")
+                    file_repos_names.write(split[3] + "/" + split[4])
+                    file_repos_names.write('\n')
+                    count += 1
+                    # if (similarity != None):
+                    #    print similarity.group(1)
+
 
 dev_files = []
 dev_files.append(open('dev/spring-projects_spring-boot.html', 'r'))
@@ -8,28 +37,11 @@ dev_files.append(open('dev/facebook_react.html', 'r'))
 dev_files.append(open('dev/nodegit_nodegit.html', 'r'))
 dev_files.append(open('dev/scipy_scipy.html', 'r'))
 
-dev_repos_urls = open('dev/dev_repos_urls.txt', 'w')
-dev_repos_names = open('dev/dev_repos_names.txt', 'a')
-
+hw_files = []
+hw_files.append(open('hw/m2mtech_calculator-2015.html', 'r'))
+hw_files.append(open('hw/bcaffo_751and2.html', 'r'))
 
 matching_urls = re.compile("data-ng-href=\"(.*)\" target")
 matching_similarity = re.compile("Similarity: (0|[1-9][0-9]*)")
 
-for file in dev_files:
-    lines = file.readlines()
-    file.close()
-    count = 0
-    for line in lines:
-        url = matching_urls.search(line)
-        similarity = matching_similarity.search(line)
-        if url != None:
-            url = url.group(1)
-            dev_repos_urls.write(url)
-            dev_repos_urls.write('\n')
-            if count < SIMILAR_RESULTS_COUNT:
-                split = url.split("/")
-                dev_repos_names.write(split[3] + "/" + split[4])
-                dev_repos_names.write('\n')
-                count+=1
-       # if (similarity != None):
-       #    print similarity.group(1)
+read_and_store_repo_names(hw_files, Labels.hw)
---
+++ data collection/labels.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class Labels(Enum):
+    dev = 'dev'
+    hw = 'hw'
+    edu = 'edu'
