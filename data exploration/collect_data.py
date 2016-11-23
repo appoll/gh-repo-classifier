f7efc08dd72fd33c5857f898d1fc994c700dda0a
FlatErik90 <efl@informatik.uni-kiel.de>
2016-11-23 10:57:09 +0100

    adjusted data collection and feature listing

--- data exploration/collect_data.py
+++ data exploration/collect_data.py
@@ -2,28 +2,18 @@ import json
 import requests
 from requests.auth import HTTPBasicAuth
 
-class ExampleData:
+def dump_repo_data(class_name):
 
-    def __init__(self):
-
-        self.repo_data = []
-
-        with open("../data collection/dev/dev_repos_names.txt", 'r') as file:
+        with open("../data collection/"+class_name+"/"+class_name+"_repos_names.txt", 'r') as file:
             repos = file.readlines()
 
-        header = {'X-Github-Username': 'FlatErik90', 'X-Github-API-Token': "36dc9f6e45d8019dafe001adf05c433f22e6782e"}
-
         for repo in repos:
-            r = requests.get("https://api.github.com/repos/"+repo[:-1],auth=HTTPBasicAuth('FlatErik90','bvb90EF'))
-            print "https://api.github.com/repos/"+repo, r.status_code, r.json()
+            r = requests.get("https://api.github.com/repos/"+repo[:-1],auth=('FlatErik90','bvb90EF'))
+            print "https://api.github.com/repos/"+repo, r.status_code
             if r.status_code == 200:
-                self.repo_data.append((r.json(), 'DEV'))
-                with open("../data collection/dev/repos/"+repo[:-1].split('/')[1]+".json",'w') as file:
-                    jsonContent = str(r.json())
+                with open("../data collection/"+class_name+"/repos/"+repo[:-1].split('/')[1]+".json",'w') as file:
+                    jsonContent = json.dumps(r.json())
                     file.write(jsonContent)
                     file.close()
 
-
-        print self.repo_data
-
-ExampleData()
+dump_repo_data("data")
--- data exploration/list_features.py
+++ data exploration/list_features.py
@@ -1,16 +1,7 @@
 import json
 import re
 from sklearn.feature_extraction import DictVectorizer
-from sklearn.decomposition import PCA
-import matplotlib.pyplot as plt
-
-# open stored meta data of an example repo
-f1 = open('DEV_homeworkr_basics.txt', 'r')
-f2 = open('DATA_housing_basics.txt', 'r')
-
-# conversion from json data types to python data types
-object1 = json.load(f1)
-object2 = json.load(f2)
+import os
 
 regexExclusions = [
     '.*url', # useless
@@ -25,39 +16,52 @@ regexExclusions = [
     'id'
 ]
 
-combinedRegexes= "(" + ")|(".join(regexExclusions) + ")"
+regexInclusions = [
+    'forks',
+    'size',
+    #'language',
+    'open_issues',
+    'stargazers_count',
+    'has_issues',
+    'has_downloads',
+    'watchers_count',
+    'subscribers_count',
+    'network_count'
+]
 
-# omit irrelevant fields like url's, dates and owner
-features1 = filter(lambda x: not(re.search(combinedRegexes,x)), object1.keys())
-features2 = filter(lambda x: not(re.search(combinedRegexes,x)), object2.keys())
+combinedRegexes= "(^" + "$)|(^".join(regexInclusions) + "$)"
+print combinedRegexes
 
-featureDict1 = dict()
-featureDict2 = dict()
+featureDicts = []
+targets = []
 
-# print all features
-#for feature in features:
-#    print str(feature)+': '+str(type(object[feature]))
+for file in os.listdir("data collection/dev/repos"):
+    if file.endswith(".json"):
+        # load file
+        repoFile = open("data collection/dev/repos/"+file,'r')
+        # decode json object
+        repo = json.load(repoFile)
+        # omit irrelevant fields like url's, dates and owner
+        features = filter(lambda x: (re.search(combinedRegexes,x)), repo.keys())
+        featureDict = dict()
+        for feature in features:
+            featureDict[feature] = repo[feature]
+        featureDicts.append(featureDict.values())
+        targets.append("DEV")
 
-for feature in features1:
-    featureDict1[feature] = object1[feature]
+for file in os.listdir("data collection/data/repos"):
+    if file.endswith(".json"):
+        # load file
+        repoFile = open("data collection/data/repos/"+file,'r')
+        # decode json object
+        repo = json.load(repoFile)
+        # omit irrelevant fields like url's, dates and owner
+        features = filter(lambda x: (re.search(combinedRegexes,x)), repo.keys())
+        featureDict = dict()
+        for feature in features:
+            featureDict[feature] = repo[feature]
+        featureDicts.append(featureDict.values())
+        targets.append("DATA")
 
-for feature in features2:
-    featureDict2[feature] = object2[feature]
-
-vec = DictVectorizer()
-print vec.fit_transform([featureDict1,featureDict2]).toarray()
-
-# pca = pca()
-# pca.fit_transform(vec.fit_transform([featuredict1,featuredict2]).toarray())
-# print pca.explained_variance_
-print vec.get_feature_names()
-#
-# plt.figure(1, figsize=(4, 3))
-# plt.clf()
-# plt.axes([.2, .2, .7, .7])
-# plt.plot(pca.explained_variance_, linewidth=2)
-# plt.axis('tight')
-# plt.xlabel('n_components')
-# plt.ylabel('explained_variance_')
-# plt.show()
-
+print featureDicts
+print targets
