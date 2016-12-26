30ccf3f742c3c782c3ec9726a03ef0c33e6ae566
appoll <paul.antton@gmail.com>
2016-12-26 14:33:08 +0100

    Add reader for github OAuth token

--- config/reader.py
+++ config/reader.py
@@ -3,9 +3,15 @@ import json
 
 class ConfigReader:
     def __init__(self):
-        github_auth_file = "../github-api-credentials.config"
-        file = open(github_auth_file, 'r')
+        github_credentials_file = "../github-api-credentials.config"
+        github_oauth_token_file = "../github-api-OAuth.config"
+        file = open(github_credentials_file, 'r')
         self.credentials = json.load(file)
+        file = open(github_oauth_token_file, 'r')
+        self.oauth = json.load(file)
 
-    def getCredentials(self):
+    def get_credentials(self):
         return self.credentials["username"], self.credentials["password"]
+
+    def get_oauth_token(self):
+        return self.oauth["token"]
