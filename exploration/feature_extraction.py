6dc489244d46aacf00a4571ee10383b0307c51d8
appoll <paul.antton@gmail.com>
2017-01-06 23:51:24 +0100

    extract basic language features

--- exploration/feature_extraction.py
+++ exploration/feature_extraction.py
@@ -1,9 +1,13 @@
+import collections
 import glob
 import json
+import math
 import os
 from datetime import datetime
 
 import dateutil.parser
+
+from collection.labels import Labels
 
 
 class FeatureExtraction:
@@ -11,17 +15,23 @@ class FeatureExtraction:
         self.additional_commits_folder = "../exploration/additional/json_commits_%s/"
         self.additional_features_folder = "../exploration/additional/features/"
 
-        self.commits_folder = "../exploration/additional/json_commits_%s/"
-        self.features_folder = "../exploration/additional/features/"
+        self.commits_folder = "../collection/%s/json_commits/"
+        self.features_folder = "../exploration/features/"
 
-    def get_commits_features(self, label):
-        folder = self.additional_commits_folder % label
-        name = self.additional_features_folder + "commit_data_%s.txt" % label
+        self.repos_folder = "../collection/%s/json_repos_updated/"
+
+    def get_commits_features(self, label, additional):
+        if additional:
+            folder = self.additional_commits_folder % label
+            name = self.additional_features_folder + "commit_data_%s.txt" % label
+        else:
+            folder = self.commits_folder % label
+            name = self.features_folder + "commit_data_%s.txt" % label
 
         if not os.path.exists(os.path.dirname(name)):
             os.makedirs(os.path.dirname(name))
         f = open(name, 'w')
-        header = "all_commits weekend_commits weekday_commits work_hrs_commits non_work_hrs_commits inter_commit_distance_average commits_per_day_average authors_count author_vs_committer repo_name\n"
+        header = "all_commits weekend_commits weekday_commits work_hrs_commits non_work_hrs_commits inter_commit_distance_average commits_per_day_average authors_count author_vs_committer active_days repo_name\n"
         f.write(header)
 
         for filename in glob.glob(folder + '*'):
@@ -40,6 +50,7 @@ class FeatureExtraction:
             inter_commit_distance = 0
             prev_date = 0
 
+            print 'Processing %s commits...' % len(commits_list)
             for commit in commits_list:
                 author_date = dateutil.parser.parse(commit['author_date'])
 
@@ -77,6 +88,7 @@ class FeatureExtraction:
             line = line + " " + "%.2f" % commits_per_day
             line = line + " " + "%.2f" % (self.get_unique_authors(commits_list))
             line = line + " " + "%.2f" % (self.get_authors_vs_committers(commits_list))
+            line = line + " " + "%.2f" % (self.get_active_days(commits_list))
 
             line = line + " " + name.split('.')[0]
 
@@ -118,13 +130,163 @@ class FeatureExtraction:
                 count += 1
         return count
 
+    def get_active_days(self, commits_list):
+        day_in_seconds = 86400
+        all_commits_count = len(commits_list)
+
+        first_commit = commits_list[all_commits_count - 1]
+        last_commit = commits_list[0]
+
+        first_commit_author_date = dateutil.parser.parse(first_commit['author_date'])
+        last_commit_author_date = dateutil.parser.parse(last_commit['author_date'])
+
+        commits_interval = last_commit_author_date - first_commit_author_date
+        days = commits_interval.total_seconds() / day_in_seconds
+
+        active_days = []
+
+        if days > 0:
+            for commit in commits_list:
+                commit_author_date = dateutil.parser.parse(commit['author_date'])
+                active_day = datetime(year=commit_author_date.year, month=commit_author_date.month,
+                                      day=commit_author_date.day)
+                if active_day not in active_days:
+                    active_days.append(active_day)
+            if len(active_days) / math.ceil(days) > 1:
+                return 1
+            return len(active_days) / math.ceil(days)
+        print 'only one commit here!'
+        return 1
+        #
+        # if days > 1:
+        #     prev_date = last_commit_author_date
+        #
+        #     active_days = 0
+        #
+        #     for commit in commits_list:
+        #         commit_author_date = dateutil.parser.parse(commit['author_date'])
+        #         print prev_date
+        #
+        #         print commit_author_date
+        #
+        #         print prev_date - timedelta(days=1)
+        #
+        #         print "ha"
+        #         if commit_author_date > prev_date:
+        #             print 'continue'
+        #             continue
+        #
+        #         if prev_date >= commit_author_date > prev_date - timedelta(days=1):
+        #             print 'active ++'
+        #             active_days += 1
+        #             dist = prev_date - commit_author_date
+        #             shift = dist.total_seconds() / day_in_seconds
+        #             print shift
+        #             print int(shift)
+        #             if shift < 1:
+        #                 prev_date = prev_date - timedelta(days=1)
+        #             else:
+        #                 prev_date = prev_date - timedelta(days=int(shift))
+        #
+        #     print active_days
+        #     print days
+        #     return active_days / math.ceil(days)
+        # return 1
+
+    def get_language_features(self, label, additional):
+        if additional:
+            folder = self.repos_folder % label
+            name = self.additional_features_folder + "languages_data_%s.txt" % label
+        else:
+            folder = self.repos_folder % label
+            name = self.features_folder + "languages_data_%s.txt" % label
+
+        all_languages = self.get_all_languages()
+
+        if not os.path.exists(os.path.dirname(name)):
+            os.makedirs(os.path.dirname(name))
+        f = open(name, 'w')
+        header = "languages_count total_bytes %s repo_name\n"
+        header = header % str(all_languages.keys())
+        f.write(header)
+
+        for filename in glob.glob(folder + '*'):
+            print filename
+            current_languages = all_languages.fromkeys(all_languages, 0)
+            json_file = open(filename, 'r')
+            name = os.path.basename(filename)
+            repo = json.load(json_file)
+
+            languages = repo["languages"]
+
+            all_languages_count = len(languages)
+            total_bytes = sum(languages.itervalues())
+
+            for language, bytes in languages.iteritems():
+                print language
+                if language not in current_languages:
+                    raise ValueError("Should not be!")
+                current_languages[language] = bytes
+
+            print current_languages
+
+            line = "%.2f" % all_languages_count
+            line = line + " " + "%.2f" % total_bytes
+            for bytes in current_languages.values():
+                line = line + " " + "%.2f" % bytes
+
+            line = line + " " + name.split('.')[0]
+
+            f.write(line)
+            f.write('\n')
+        print "Wrote languages features to %s" % f.name
+        f.close()
+
+    def get_all_languages(self):
+        used_languages = {}
+        for label in Labels.toArray():
+            print label
+            folder = self.repos_folder % label.value
+            for filename in glob.glob(folder + '*'):
+                print filename
+                json_file = open(filename, 'r')
+                repo = json.load(json_file)
+
+                try:
+                    languages = repo["languages"]
+                except KeyError:
+                    continue
+
+                for language in languages:
+                    if language not in used_languages:
+                        used_languages[language] = 0
+                        print 'Added %s to the list of used languages' % language
+
+        return collections.OrderedDict(used_languages)
+
 
 featureExtraction = FeatureExtraction()
-featureExtraction.get_commits_features('docs')
+# featureExtraction.get_commits_features('docs', additional=True)
+# featureExtraction.get_commits_features('dev', additional=True)
+# featureExtraction.get_commits_features('data', additional=True)
+# featureExtraction.get_commits_features('hw', additional=True)
+# featureExtraction.get_commits_features('edu', additional=True)
+# featureExtraction.get_commits_features('web', additional=True)
+# featureExtraction.get_commits_features('other', additional=True)
 
-featureExtraction.get_commits_features('dev')
-featureExtraction.get_commits_features('data')
-featureExtraction.get_commits_features('hw')
-featureExtraction.get_commits_features('edu')
-featureExtraction.get_commits_features('web')
-featureExtraction.get_commits_features('other')
+# collect docs commits again
+
+# featureExtraction.get_commits_features('docs', additional=False)
+
+# success:
+# featureExtraction.get_commits_features('dev', additional=False)
+# featureExtraction.get_commits_features('data', additional=False)
+# featureExtraction.get_commits_features('hw', additional=False)
+# featureExtraction.get_commits_features('edu', additional=False)
+# featureExtraction.get_commits_features('web', additional=False)
+# featureExtraction.get_commits_features('other', additional=False)
+
+# must all be called together
+featureExtraction.get_language_features('docs', additional=False)
+
+# featureExtraction.get_all_languages()
