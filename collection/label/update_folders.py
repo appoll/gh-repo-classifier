b754a19767d840a9663be29c7bcca4c4e9f9241c
appoll <paul.antton@gmail.com>
2017-01-09 12:05:13 +0100

    move not only original label, but all manually confirmed labels

--- collection/label/update_folders.py
+++ collection/label/update_folders.py
@@ -5,22 +5,40 @@ import shutil
 from collection.labels import Labels
 
 
-def update(label, source, dest):
-    source = source % label
-    dest = dest % label
-
-    labelled_file = "../labelled_%s" % label
-
-    file = open(labelled_file, 'r')
+def extract_labelled_repos_names(labelled):
+    file = open(labelled, 'r')
     labelled_repos = file.readlines()
 
     labelled_repos_names = []
 
     for line in labelled_repos:
         labelled_repos_names.append(line.rstrip('\n'))
+    return labelled_repos_names
 
-    total = len(labelled_repos_names)
-    count = 0
+
+def update(label, source, dest):
+    """
+    :param label: the label which has been manually validated
+    :param source: the source folder where repo info already exists
+    :param dest: the destination folder of labelled repo info
+    """
+    source = source % label
+    labelled_web = "../labelled_web"
+    labelled_dev = "../labelled_dev"
+    labelled_data = "../labelled_data"
+    labelled_docs = "../labelled_docs"
+    labelled_edu = "../labelled_edu"
+    labelled_hw = "../labelled_hw"
+    labelled_other = "../labelled_other"
+
+    dev_labelled_repos_names = extract_labelled_repos_names(labelled_dev)
+    data_labelled_repos_names = extract_labelled_repos_names(labelled_data)
+    docs_labelled_repos_names = extract_labelled_repos_names(labelled_docs)
+    edu_labelled_repos_names = extract_labelled_repos_names(labelled_edu)
+    hw_labelled_repos_names = extract_labelled_repos_names(labelled_hw)
+    other_labelled_repos_names = extract_labelled_repos_names(labelled_other)
+    web_labelled_repos_names = extract_labelled_repos_names(labelled_web)
+
     for filename in glob.glob(source + '*'):
         split = filename.split('/')
         repo_name_json = split[len(split) - 1]
@@ -39,17 +57,29 @@ def update(label, source, dest):
         else:
             raise Exception('should not be!')
 
-        if repo_name in labelled_repos_names:
-            if not os.path.exists(os.path.dirname(dest)):
-                os.makedirs(os.path.dirname(dest))
-            shutil.copy(filename, dest)
-            print 'copied to %s' % dest
-            count += 1
+        if repo_name in dev_labelled_repos_names:
+            dest_folder = dest % 'dev'
+        elif repo_name in data_labelled_repos_names:
+            dest_folder = dest % 'data'
+        elif repo_name in docs_labelled_repos_names:
+            dest_folder = dest % 'docs'
+        elif repo_name in edu_labelled_repos_names:
+            dest_folder = dest % 'edu'
+        elif repo_name in hw_labelled_repos_names:
+            dest_folder = dest % 'hw'
+        elif repo_name in other_labelled_repos_names:
+            dest_folder = dest % 'other'
+        elif repo_name in web_labelled_repos_names:
+            dest_folder = dest % 'web'
         else:
-            pass
-            #print 'Not there, not copying.'
+            print 'not labelled'
+            continue
 
-    print 'copied %d out of %d for the %s label' % (count, total, label)
+        if not os.path.exists(os.path.dirname(dest_folder)):
+            os.makedirs(os.path.dirname(dest_folder))
+        shutil.copy(filename, dest_folder)
+        print 'copied to %s folder' % dest_folder
+
 
 repos_folder = "../%s/json_repos_unarchived/"
 repos_folder_updated = "../%s/json_repos_updated/"
@@ -59,31 +89,6 @@ repos_folder_labelled = "../%s/json_repos_unarchived_labelled/"
 repos_folder_updated_labelled = "../%s/json_repos_updated_labelled/"
 readmes_repos_folder_labelled = "../%s/json_readmes_unarchived_labelled/"
 
-# update(Labels.web.value, repos_folder, repos_folder_labelled)
-# update(Labels.web.value, repos_folder_updated, repos_folder_updated_labelled)
-# update(Labels.web.value, readmes_repos_folder, readmes_repos_folder_labelled)
-
-
-update(Labels.dev.value, repos_folder, repos_folder_labelled)
-update(Labels.dev.value, repos_folder_updated, repos_folder_updated_labelled)
-update(Labels.dev.value, readmes_repos_folder, readmes_repos_folder_labelled)
-
-update(Labels.data.value, repos_folder, repos_folder_labelled)
-update(Labels.data.value, repos_folder_updated, repos_folder_updated_labelled)
-update(Labels.data.value, readmes_repos_folder, readmes_repos_folder_labelled)
-
-update(Labels.docs.value, repos_folder, repos_folder_labelled)
-update(Labels.docs.value, repos_folder_updated, repos_folder_updated_labelled)
-update(Labels.docs.value, readmes_repos_folder, readmes_repos_folder_labelled)
-
-update(Labels.hw.value, repos_folder, repos_folder_labelled)
-update(Labels.hw.value, repos_folder_updated, repos_folder_updated_labelled)
-update(Labels.hw.value, readmes_repos_folder, readmes_repos_folder_labelled)
-
-update(Labels.uncertain.value, repos_folder, repos_folder_labelled)
-update(Labels.uncertain.value, repos_folder_updated, repos_folder_updated_labelled)
-update(Labels.uncertain.value, readmes_repos_folder, readmes_repos_folder_labelled)
-
-update(Labels.edu.value, repos_folder, repos_folder_labelled)
-update(Labels.edu.value, repos_folder_updated, repos_folder_updated_labelled)
-update(Labels.edu.value, readmes_repos_folder, readmes_repos_folder_labelled)
+update(Labels.web.value, repos_folder, repos_folder_labelled)
+update(Labels.web.value, repos_folder_updated, repos_folder_updated_labelled)
+update(Labels.web.value, readmes_repos_folder, readmes_repos_folder_labelled)
