b1ef7984eea0d38c95d6aad8f09505a840c99ce0
appoll <paul.antton@gmail.com>
2017-01-10 12:30:25 +0100

    multiclass clf: compare average accuracy and precision for non-optimized Linear and rbf SVC

--- classification/multiclass/language_data.py
+++ classification/multiclass/language_data.py
@@ -1,19 +1,16 @@
-import numpy as np
 import pandas as pd
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.model_selection import train_test_split
+from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
 
-from classification import svc_linear
 from collection.labels import Labels
 
 
-def content(label):
-    features = pd.read_csv("../exploration/features/languages_data_%s.txt" % label.value, delimiter=" ", header=0)
-    print("./exploration/features/languages_data_%s.txt" % label.value)
-    return features
-
-
 def features(label):
-    features = pd.read_csv("../exploration/features/languages_data_%s.txt" % label.value, delimiter=" ", header=0)
-    print("./exploration/features/languages_data_%s.txt" % label.value)
+    features = pd.read_csv("../../exploration/labelled/features/languages_data_%s.txt" % label.value, delimiter=" ",
+                           header=0)
 
     print features.shape
 
@@ -37,21 +34,55 @@ def features(label):
     return features
 
 
-data = [features(Labels.edu), features(Labels.data), features(Labels.hw), features(Labels.web), features(Labels.dev),
+features = [features(Labels.edu), features(Labels.data), features(Labels.hw), features(Labels.web), features(Labels.dev),
         features(Labels.docs)]
 
-data = pd.concat(data)
+data = pd.concat(features)
 
-train_data = data
-train_data = train_data[(train_data.T != 0).any()]
-labels = train_data['label']
-train_data = train_data.drop(labels='label', axis=1)
+linear_svc = LinearSVC()
 
-print data.shape
-print train_data.shape
-print labels.shape
+rbf_svc = SVC(kernel='rbf')
 
-training_score, testing_score = svc_linear.run(train_data, labels)
+# sample 100 times and get average performance scores
+linear_svc_precision_sum = 0
+linear_svc_accuracy_sum = 0
 
-print training_score
-print testing_score
+rbf_svc_precision_sum = 0
+rbf_svc_accuracy_sum = 0
+
+sample_runs = 100
+for i in range(sample_runs):
+    labels = data['label']
+    trimmed_data = data.drop(labels='label', axis=1)
+
+    x_train, x_test, y_train, y_test = train_test_split(trimmed_data, labels, random_state=0)
+
+    linear_svc.fit(x_train, y_train)
+    rbf_svc.fit(x_train, y_train)
+
+    y_pred_linear = linear_svc.predict(x_test)
+    y_pred_rbf = rbf_svc.predict(x_test)
+
+    linear_svc_precision = precision_score(y_test, y_pred_linear, average='micro')
+    linear_svc_precision_sum += linear_svc_precision * 100
+    rbf_svc_precision = precision_score(y_test, y_pred_rbf, average='micro')
+    rbf_svc_precision_sum += rbf_svc_precision * 100
+
+    linear_svc_accuracy = accuracy_score(y_test, y_pred_linear)
+    linear_svc_accuracy_sum += linear_svc_accuracy * 100
+    rbf_svc_accuracy = accuracy_score(y_test, y_pred_rbf)
+    rbf_svc_accuracy_sum += rbf_svc_accuracy * 100
+
+linear_svc_accuracy_average = linear_svc_accuracy_sum / sample_runs
+linear_svc_precision_average = linear_svc_precision_sum / sample_runs
+
+rbf_svc_accuracy_average = rbf_svc_accuracy_sum / sample_runs
+rbf_svc_precision_average = rbf_svc_precision_sum / sample_runs
+
+print "Linear SVC: average accuracy and precision"
+print linear_svc_accuracy_average
+print linear_svc_precision_average
+
+print "rbf SVC: average accuracy and precision"
+print rbf_svc_accuracy_average
+print rbf_svc_precision_average
