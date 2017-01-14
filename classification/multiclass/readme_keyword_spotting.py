e9ce0b88c7b3d30dd5678e13478afe4a3db9bd7a
hummelth <thm10230@gmail.com>
2017-01-14 21:46:42 +0100

    updated readme-keyword classifier

--- classification/multiclass/readme_keyword_spotting.py
+++ classification/multiclass/readme_keyword_spotting.py
@@ -5,32 +5,48 @@ import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
 import nltk
+from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import precision_score
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
+from collection.labels import Labels
 
 from sklearn.feature_extraction.text import CountVectorizer
 
 train = pd.read_csv("../../exploration/text_data.txt", delimiter=" ", header=0)
+
+
 # Download stopwords corpus
 nltk.download('stopwords')
 
 # keywords_edu = ["course", "slide", "lecture", "assignment", "university", "student", "week", "schedule",
 #                 "work"]
+
+
+
 keywords_edu = ["course", "coursera", "slide", "lecture", "assignment", "university", "student", "week", "schedule",
                 "work", "term", "condition", "education", "class"]
-keywords_dev = ["library", "package", "framework", "module", "app", "application", "server", "license", "develop", "installation"]
-# keywords_dev = ["library", "package", "framework", "module", "app", "server"]
-
+keywords_dev = ["library", "package", "framework", "module", "app", "application", "server", "license", "develop",
+                "dependencies", "installation", "api", "client"]
 keywords_data = ["data", "dataset", "sample", "set", "database", "table"]
-# keywords_data = ["data", "sample", "set", "table"]
 keywords_hw = ["homework", "solution"]
-
 keywords_web = ["web", "website", "homepage", "javascript"]
 keywords_doc = ["documentation", "collection", "manuals", "docs"]
+
+
+
+# keywords_edu = ["course", "slide", "lecture", "assignment", "eduation"]
+# keywords_dev = ["scripts", "pom.xml", "framework", "install"]
+# keywords_data = ["dataset"]
+# keywords_hw = ["homework", "hw0", "hw1", "task", "lesson", "week_"]
+# keywords_web = ["website"]
+# keywords_doc = ["doc"]
+
+
+
 
 keyword_list = []
 keyword_list.extend(keywords_edu)
@@ -77,15 +93,72 @@ def print_feature_matrix(features, withCorrespondingExample=False):
     else:
         print features
 
-def readme_keyword_spotting(readme_content, keyword_list):
+def keyword_spotting(content, keyword_list):
     # init binary vector with zeros
     binary_vector = np.zeros(len(keyword_list))
     for index, key in enumerate(keyword_list):
-        readme_set = readme_content.split(" ")
-        for word in readme_set:
+        word_set = content.split(" ")
+        for word in word_set:
             if key in word:
                 binary_vector[index] = 1
     return binary_vector
+
+
+def read_contents_data(label):
+    features = pd.read_csv("../../exploration/labelled/features/contents_data_%s.txt" % label, delimiter=" ", header=0)
+
+    print features.shape
+
+    #features.to_csv('contents_repo_names_%s' % label, columns=["repo_name"])
+    # features = features.drop(labels='repo_name', axis=1)
+
+    print features.shape
+
+    if label == Labels.data:
+        features['label'] = 0
+    elif label == Labels.dev:
+        features['label'] = 1
+    elif label == Labels.docs:
+        features['label'] = 2
+    elif label == Labels.edu:
+        features['label'] = 3
+    elif label == Labels.hw:
+        features['label'] = 4
+    elif label == Labels.web:
+        features['label'] = 5
+    elif label == Labels.uncertain:
+        features['label'] = 6
+
+    return features
+
+def extract_contents():
+    data = [read_contents_data(Labels.data), read_contents_data(Labels.dev), read_contents_data(Labels.docs), read_contents_data(Labels.edu),
+                read_contents_data(Labels.hw), read_contents_data(Labels.web), read_contents_data(Labels.uncertain)]
+
+    data = pd.concat(data)
+    print data.shape
+    contents = data["fo_and_fi_names"]
+    labels = data['label']
+    return contents, labels
+
+def extract_all_contents():
+    data = [read_contents_data(Labels.data), read_contents_data(Labels.dev), read_contents_data(Labels.docs), read_contents_data(Labels.edu),
+                read_contents_data(Labels.hw), read_contents_data(Labels.web), read_contents_data(Labels.uncertain)]
+
+    data = pd.concat(data)
+    return data
+
+# data = extract_all_contents()
+#
+# print np.shape(data)
+# print np.shape(train)
+#
+# test = train.merge(data, on="repo_name", how="outer")
+#
+# test.to_csv("trash_data.txt", sep=" ")
+
+X = []
+
 
 rows = train['readme_filename'].size
 clean_readmes = []
@@ -104,25 +177,43 @@ for i in xrange(0, rows):
     clean_readmes.append(raw_to_words(content))
 
 
-X = []
-for readme in clean_readmes:
-    keys = readme_keyword_spotting(readme, keyword_list=keyword_list)
+
+for repository_readme in clean_readmes:
+    keys = keyword_spotting(repository_readme, keyword_list=keyword_list)
     X.append(keys)
 
 labels = train['label']
+
+
+
+# contents, labels = extract_contents()
+#
+# # labels = np.concatenate((labels,labels_content))
+#
+# for repository_content in contents:
+#     keys = keyword_spotting(repository_content, keyword_list=keyword_list)
+#     X.append(keys)
+
+
+
+
+
+
 Y = np.asarray(labels, dtype=int)
 X = np.array(X)
 print X.shape
 print Y.shape
-
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
 
-clf = RandomForestClassifier(n_estimators=1000, n_jobs=-1, random_state=0, max_depth=30)
+
+
+clf = RandomForestClassifier(n_estimators=1000, n_jobs=-1, random_state=1, max_depth=30)
 clf.fit(X_train, Y_train)
 
 output = clf.predict(X_test)
 
 score = precision_score(Y_test, output, average=None)
+
 print score
 
 print np.mean(score)
