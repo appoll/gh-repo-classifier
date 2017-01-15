017d2edb54c6ccb8e8c11a2a1b600b37ee22634e
appoll <paul.antton@gmail.com>
2017-01-15 13:48:12 +0100

    ensemble attempt 1: merge feature vectors. try different classifiers

--- classification/multiclass/repo_data.py
+++ classification/multiclass/repo_data.py
@@ -1,13 +1,17 @@
 import numpy as np
 import pandas as pd
+from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import VotingClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-from sklearn.metrics import mean_squared_error
 from sklearn.metrics import precision_score
 from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
 
 from collection.labels import Labels
-from config.helper import Helper
 
 
 def get_features(label):
@@ -57,11 +61,74 @@ print test_data.shape
 forest_classifier = RandomForestClassifier(n_estimators=500, max_depth=5, max_features=3)
 forest = forest_classifier.fit(train_data, train_labels)
 
-output = forest.predict(test_data)
-print mean_squared_error(output, test_labels)
+svc_classifier = SVC(kernel="rbf", probability=True)
+# C_range = np.logspace(-2, 10, 13)
+# gamma_range = np.logspace(-9, 3, 13)
+#
+# tuned_parameters = [{'kernel': ['rbf'], 'gamma': gamma_range,
+#                          'C': C_range}]
+#
+# svc_classifier = GridSearchCV(svc_classifier, tuned_parameters, scoring='precision_macro', n_jobs=-1)
+
+svc = svc_classifier.fit(train_data, train_labels)
+
+knn_classifier = KNeighborsClassifier(n_neighbors=8)
+knn = knn_classifier.fit(train_data, train_labels)
+
+gaussian_classifier = GaussianNB()
+gaussian = gaussian_classifier.fit(train_data, train_labels)
+
+logistic_regression_clf = LogisticRegression()
+log_reg = logistic_regression_clf.fit(train_data, train_labels)
+
+ada_boost_clf = AdaBoostClassifier()
+ada_boost = ada_boost_clf.fit(train_data, train_labels)
+
+voting_classifier = VotingClassifier(estimators=[('fc', forest), ('knn', knn), ('rbf', svc), ('gaussian', gaussian)],
+                                     voting='soft')
+voting = voting_classifier.fit(train_data, train_labels)
+
+svc_output = svc.predict(test_data)
+score = precision_score(test_labels, svc_output, average=None)
+print '\nsvc'
+print score
+print np.mean(score)
+
+knn_output = knn.predict(test_data)
+score = precision_score(test_labels, knn_output, average=None)
+print '\nknn'
+print score
+print np.mean(score)
+
+forest_output = forest.predict(test_data)
+score = precision_score(test_labels, forest_output, average=None)
+print '\nforest'
+print score
+print np.mean(score)
+
+gaussian_output = gaussian.predict(test_data)
+score = precision_score(test_labels, gaussian_output, average=None)
+print '\ngaussian'
+print score
+print np.mean(score)
+
+log_reg_output = log_reg.predict(test_data)
+score = precision_score(test_labels, log_reg_output, average=None)
+print '\nlogistic regression'
+print score
+print np.mean(score)
+
+ada_boost_output = ada_boost.predict(test_data)
+score = precision_score(test_labels, ada_boost_output, average=None)
+print '\nAda Boost'
+print score
+print np.mean(score)
+
+output = voting.predict(test_data)
+print '\nvoting'
 print accuracy_score(test_labels, output)
 score = precision_score(test_labels, output, average=None)
 print score
 print np.mean(score)
 
-Helper().write_probabilities(forest, data, repo_names, 'prob/prob_repo_data')
+# Helper().write_probabilities(forest, data, repo_names, 'prob/prob_repo_data')
