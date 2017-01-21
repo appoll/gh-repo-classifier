2d29628bb722345c3c06e4dcaf4cccff9f8bf6a6
hummelth <thm10230@gmail.com>
2017-01-21 19:26:41 +0100

    feature preparation can be now executed on training files (/exploration/labelled/features/) or on prediction (/prediction/features/)

--- classification/multiclass/executor.py
+++ classification/multiclass/executor.py
@@ -6,6 +6,8 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, mean_
 from sklearn.model_selection import train_test_split
 from sklearn.externals import joblib
 
+from settings import FEATURE_TRAIN_PATH
+from settings import FEATURE_PREDICT_PATH
 from config.constants import *
 from config.helper import Helper
 from keyword_spotting import KeywordSpotting
@@ -13,9 +15,8 @@ from solid_classifier import SolidClassifier
 
 from collection.labels import Labels
 
-FEATURE_TRAIN_LOCATION = "../../exploration/labelled/features/"
-
-FEATURE_DATA_FORMAT = "%s_data_%s.txt"
+FEATURE_DATA_FORMAT_TRAIN = "%s_data_%s.txt"
+FEATURE_DATA_FORMAT_PREDICT = "%s_data.txt"
 
 REPO = "repo"
 CI = "commits_interval"
@@ -25,91 +26,50 @@ README = "readme"
 CONTENTS = "contents"
 TREES = "trees"
 
+LABELS = [Labels.data, Labels.dev, Labels.docs, Labels.edu, Labels.hw, Labels.web, Labels.uncertain]
 
 class FeaturePreparation():
-    def __init__(self):
-        # self.clf = RandomForestClassifier(n_estimators=10000, max_depth=60)
-        self.folder_path_data = FEATURE_TRAIN_LOCATION
+    def __init__(self, is_train = False):
+        self.is_train = is_train
 
-    def read_features_from_file(self, label, which):
-        path = self.folder_path_data + FEATURE_DATA_FORMAT % (which, label)
-        features = pd.read_csv(path, delimiter=" ", header=0, skipfooter=1)
-        print path
-        print features.shape
+    def read_features_from_file(self, which):
+        features_list = []
+        if self.is_train:
+            for label in LABELS:
+                path = FEATURE_TRAIN_PATH + FEATURE_DATA_FORMAT_TRAIN % (which, label)
+                features = pd.read_csv(path, delimiter=" ", header=0, skipfooter=1)
+                print path
+                print features.shape
 
-        # features.to_csv('repo_repo_names_%s' % label, columns=["repo_name"])
+                if label == Labels.data:
+                    features['label'] = 0
+                elif label == Labels.dev:
+                    features['label'] = 1
+                elif label == Labels.docs:
+                    features['label'] = 2
+                elif label == Labels.edu:
+                    features['label'] = 3
+                elif label == Labels.hw:
+                    features['label'] = 4
+                elif label == Labels.web:
+                    features['label'] = 5
+                elif label == Labels.uncertain:
+                    features['label'] = 6
+                features_list.append(features)
+        else:
+            path = FEATURE_PREDICT_PATH + FEATURE_DATA_FORMAT_PREDICT % (which)
+            features_list = [pd.read_csv(path, delimiter=" ", header=0, skipfooter=1)]
+        return features_list
 
-        if label == Labels.data:
-            features['label'] = 0
-        elif label == Labels.dev:
-            features['label'] = 1
-        elif label == Labels.docs:
-            features['label'] = 2
-        elif label == Labels.edu:
-            features['label'] = 3
-        elif label == Labels.hw:
-            features['label'] = 4
-        elif label == Labels.web:
-            features['label'] = 5
-        elif label == Labels.uncertain:
-            features['label'] = 6
-        return features
 
     def build_feature_data(self):
-
-        repo_features = [self.read_features_from_file(Labels.data, REPO),
-                         self.read_features_from_file(Labels.dev, REPO),
-                         self.read_features_from_file(Labels.docs, REPO),
-                         self.read_features_from_file(Labels.edu, REPO),
-                         self.read_features_from_file(Labels.hw, REPO),
-                         self.read_features_from_file(Labels.web, REPO),
-                         self.read_features_from_file(Labels.uncertain, REPO)]
-
-        ci_features = [self.read_features_from_file(Labels.data, CI),
-                       self.read_features_from_file(Labels.dev, CI),
-                       self.read_features_from_file(Labels.docs, CI),
-                       self.read_features_from_file(Labels.edu, CI),
-                       self.read_features_from_file(Labels.hw, CI),
-                       self.read_features_from_file(Labels.web, CI),
-                       self.read_features_from_file(Labels.uncertain, CI)]
-
-        lang_features = [self.read_features_from_file(Labels.data, LANG),
-                         self.read_features_from_file(Labels.dev, LANG),
-                         self.read_features_from_file(Labels.docs, LANG),
-                         self.read_features_from_file(Labels.edu, LANG),
-                         self.read_features_from_file(Labels.hw, LANG),
-                         self.read_features_from_file(Labels.web, LANG),
-                         self.read_features_from_file(Labels.uncertain, LANG)]
-
-        commit_features = [self.read_features_from_file(Labels.data, COMMIT),
-                           self.read_features_from_file(Labels.dev, COMMIT),
-                           self.read_features_from_file(Labels.docs, COMMIT),
-                           self.read_features_from_file(Labels.edu, COMMIT),
-                           self.read_features_from_file(Labels.hw, COMMIT),
-                           self.read_features_from_file(Labels.web, COMMIT),
-                           self.read_features_from_file(Labels.uncertain, COMMIT)]
-
-        readme_features = [self.read_features_from_file(Labels.data, README),
-                           self.read_features_from_file(Labels.dev, README),
-                           self.read_features_from_file(Labels.docs, README),
-                           self.read_features_from_file(Labels.edu, README),
-                           self.read_features_from_file(Labels.hw, README),
-                           self.read_features_from_file(Labels.web, README),
-                           self.read_features_from_file(Labels.uncertain, README)]
-
-        # trees_features = [self.read_features_from_file(Labels.data, TREES), self.read_features_from_file(Labels.dev, TREES),
-        #                    self.read_features_from_file(Labels.docs, TREES),
-        #                    self.read_features_from_file(Labels.edu, TREES),
-        #                    self.read_features_from_file(Labels.hw, TREES), self.read_features_from_file(Labels.web, TREES),
-        #                    self.read_features_from_file(Labels.uncertain, TREES)]
-
-        contents_features = [self.read_features_from_file(Labels.data, CONTENTS),
-                             self.read_features_from_file(Labels.dev, CONTENTS),
-                             self.read_features_from_file(Labels.docs, CONTENTS),
-                             self.read_features_from_file(Labels.edu, CONTENTS),
-                             self.read_features_from_file(Labels.hw, CONTENTS),
-                             self.read_features_from_file(Labels.web, CONTENTS),
-                             self.read_features_from_file(Labels.uncertain, CONTENTS)]
+        repo_features = self.read_features_from_file(REPO)
+        ci_features = self.read_features_from_file(CI)
+        lang_features = self.read_features_from_file(LANG)
+        commit_features = self.read_features_from_file(COMMIT)
+        readme_features = self.read_features_from_file(README)
+        contents_features = self.read_features_from_file(CONTENTS)
+        # trees_features = self.read_features_from_file(TREES)
 
         repo_data = pd.concat(repo_features)
         ci_data = pd.concat(ci_features)
@@ -118,7 +78,6 @@ class FeaturePreparation():
         readme_data = pd.concat(readme_features)
         contents_data = pd.concat(contents_features)
         # trees_data = pd.concat(trees_features)
-
 
         self.repo_data = repo_data.drop_duplicates(subset=['repo_name'])
         self.ci_data = ci_data.drop_duplicates(subset=['repo_name'])
@@ -158,10 +117,9 @@ class FeaturePreparation():
         data = self.align_feature_data()
         return data
 
-
 class ClassificationExecutor():
     def __init__(self):
-        self.feature_prep = FeaturePreparation()
+        self.feature_prep = FeaturePreparation(is_train=True)
         self.data = self.feature_prep.build_data()
         return
 
