5d85087f668eafc228a163c8d75e39c3130c26d5
hummelth <thm10230@gmail.com>
2017-01-21 17:57:05 +0100

    add settings file for handling paths (system independent)

---
+++ settings.py
@@ -0,0 +1,7 @@
+import os
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+MODEL_PATH = os.path.join(PROJECT_ROOT, "models", "")
+
+STOPWORDS_PATH = os.path.join(PROJECT_ROOT, "config", "stopwords", "")
